c47e8950b04442f3aa57a5e79920ee02b7a4b39b
Nastya <malkusha@mail.ru>
2020-10-02 02:28:43 +0300

    something_else

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-fetch('https://api.github.com/users/6thSence')
+let url = window.location.href;
+console.log(url);
+async function getInfo() {
+  let info = await fetch('https://api.github.com/users/6thSence');
+  let content = await info.json();
+  console.log(content);
+};
+getInfo();
